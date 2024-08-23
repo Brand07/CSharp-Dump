7b9e71255b2198017799dee76a2197a35db51709
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-23 09:16:48 -0500

    Add comments

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,12 @@
 ﻿// Fizz Buzz Challenge
+/*
+ * From numbers 1 to 100, if the number is evenly
+ * divisible by 3, print Fizz.
+ * If the number is evenly divisible by 5, print buzz.
+ * If the number is evenly divisible by 3 and 5, print FizzBuzz
+ * 
+ */
+
 for (int number = 1; number <= 100; number++)
 {
     if (number % 3 == 0 && number % 5 == 0)
