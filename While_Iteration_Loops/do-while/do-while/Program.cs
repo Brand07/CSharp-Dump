1bc80713c00f5dfb23f2db23df6f6fb632fc93bb
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-23 09:59:03 -0500

    Add Example three

--- While_Iteration_Loops/do-while/do-while/Program.cs
+++ While_Iteration_Loops/do-while/do-while/Program.cs
@@ -6,6 +6,7 @@
 */
 
 // Write a do-while statement to break when a certain random number is generated
+Console.WriteLine("Example One:\n");
 Random randomOne = new Random();
 int currentOne = 0;
 
@@ -19,6 +20,7 @@ Console.WriteLine("End of example One\n\n");
 
 // Write a while statement that iterates only
 // while a random number is greater than some value
+Console.WriteLine("Example Two\n");
 Random randomTwo = new Random();
 int currentTwo = randomTwo.Next(1, 11);
 
@@ -28,3 +30,18 @@ while (currentTwo >= 3)
  currentTwo = randomTwo.Next(1, 11);
 }
 Console.WriteLine($"Last number: {currentTwo}");
+Console.WriteLine("End of example two\n\n");
+
+// Example three - Use the continue statement to step directly to the Boolean expression.
+Console.WriteLine("Example three:\n");
+Random randomThree = new Random();
+int currentThree = randomThree.Next(1, 11);
+
+do
+{
+ currentThree = randomThree.Next(1, 11);
+ if (currentThree >= 8) continue;
+ Console.WriteLine(currentThree);
+} while (currentThree != 7);
+Console.WriteLine("End of example three\n\n");
+
