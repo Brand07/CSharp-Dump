5b82876b9e429e7918fb85bf702ddc69738591a6
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-23 09:44:00 -0500

    Add simple example

--- While_Iteration_Loops/do-while/do-while/Program.cs
+++ While_Iteration_Loops/do-while/do-while/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+* The do-while statement executes a statement or a block of statements while a
+ * specified Boolean expression evaluates to true.
+ * Because that expression is evaluated after each execution of the loop,
+ * a do-while loop executes one or more times.
+*/
 
-Console.WriteLine("Hello, World!");
+// Write a do-while statement to break when a certain random number is generated
+Random random = new Random();
+int current = 0;
+
+do
+{
+ current = random.Next(1, 11);
+ Console.WriteLine(current);
+
+} while (current != 7);
