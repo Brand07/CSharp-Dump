f3b60f6360e16e665e44f658a2fac1a68020cce7
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-23 09:14:32 -0500

    Classic FizzBuzz Challenge

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿// Fizz Buzz Challenge
+for (int number = 1; number <= 100; number++)
+{
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+        Console.WriteLine($"{number} - FizzBuzz");
+    }
+    else if (number % 3 == 0)
+    {
+        Console.WriteLine($"{number} - Fizz");
+    }
+    else if (number % 5 == 0)
+    {
+        Console.WriteLine($"{number} - Buzz");
+    }
+    else
+    {
+        Console.WriteLine(number);
+    }
+}
