fe544dd4f798867a48fb05688b04b8024406bd83
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-23 21:58:30 -0500

    Complete Challenge 3

--- do_vs_while/Challenge_Exercise/Program.cs
+++ do_vs_while/Challenge_Exercise/Program.cs
@@ -73,3 +73,35 @@ do
   }
  }
 } while (validString == false);
+
+// Challenge Project 3 - Write code that processes the contents of a string array
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int stringCount = myStrings.Length;
+
+string myString = "";
+int periodLocation = 0;
+
+for (int i = 0; i < stringCount; i++)
+{
+ myString = myStrings[i];
+ periodLocation = myString.IndexOf(".");
+
+ string mySentence;
+
+ while (periodLocation != -1)
+ {
+  mySentence = myString.Remove(periodLocation + 1);
+  
+  myString = myString.Substring(periodLocation + 1);
+  
+  // Remove leading space
+  myString = myString.TrimStart();
+  
+  //update the comma location
+  periodLocation = myString.IndexOf(".");
+  Console.WriteLine(mySentence);
+ }
+
+ mySentence = myString.TrimStart();
+ Console.WriteLine(mySentence);
+}
