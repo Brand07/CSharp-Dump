aa81e5fe27f80e422e88ba41838e7fda092422ee
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-23 21:29:28 -0500

    Complete Challenge Project 1

--- do_vs_while/Challenge_Exercise/Program.cs
+++ do_vs_while/Challenge_Exercise/Program.cs
@@ -27,3 +27,28 @@ do
 
 // Challenge Project
 // https://learn.microsoft.com/en-us/training/modules/csharp-do-while/5-exercise-challenge-differentiate-while-do-statements
+
+// Code Project 1 - Write code that validates integer input
+int numericValue = 0;
+bool validNumber = false;
+Console.WriteLine("Enter an integer between 5 and 10:");
+do
+{
+ string? input = Console.ReadLine();
+ if (int.TryParse(input, out numericValue))
+ {
+  if (numericValue >= 5 && numericValue <= 10)
+  {
+   validNumber = true;
+   Console.WriteLine($"Your input of {numericValue} is valid.");
+  }
+  else
+  {
+   Console.WriteLine("Sorry, you entered an invalid integer.");
+  }
+ }
+ else
+ {
+  Console.WriteLine("Please enter a valid integer.");
+ }
+} while (validNumber == false);
